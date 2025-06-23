f84ef370d694cc3fada350bc93ccf2a0cce0c920
Jabi <michal.jabi@gmail.com>
2025-06-23 12:57:56 +0200

    feat(b10-wu): make it work.

--- src/B-objectivity/10-object-initialization/b10-1st-object-initialization-warm-up.js
+++ src/B-objectivity/10-object-initialization/b10-1st-object-initialization-warm-up.js
@@ -10,6 +10,62 @@ import { assertThat } from '../../j4b1-assert.js'
  * - musisz dopisać cały kod, potrzebny do poprawnego działania przypadków testowych
  */
 
+const myItCrowd = {
+	characters: {
+		list: ['Maurice', 'Jen', 'Roy']
+	},
+	office: {
+		answerPhone() {
+			return 'Have you tried to turn it off and on again?'
+		}
+	}
+}
+
+console.log([] === []);
+console.log({} === {});
+console.log(new Object() === new Object());
+console.log(new Date() === new Date());
+
+
+console.log(1n === 1n);
+console.log('A' === 'A');
+
+// symbol jest unikatowy dlatego false tutaj:
+console.log(Symbol() === Symbol())
+
+// console.log(Symbol('dev'))
+// console.log(Symbol('dev2'))
+
+
+console.log(typeof '');
+console.log(typeof 1);
+console.log(typeof true);
+
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof new Date());
+
+
+// BUG:
+console.log(typeof null);
+
+
+// vs Java:
+console.log(new Date() instanceof Date);
+console.log(new Date() instanceof Object);
+
+// Matrix:
+// const x = {}; // literał obiektowy
+// -----
+// const x = new Object();
+
+// const y = []; // literał tablicowy
+// ------
+// const y = new Array();
+
+// const z = /\d/; // literał wyrażenia regularnego
+// ------
+// const z = new RegExp("\\d");
 
 // #Reguła:
 // Nie możesz zmieniać kodu poniżej:
