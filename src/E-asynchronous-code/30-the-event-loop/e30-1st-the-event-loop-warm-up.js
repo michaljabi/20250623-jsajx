6ae3c42c8faf15145458cbe60777747e02064f45
Jabi <michal.jabi@gmail.com>
2025-06-24 12:50:36 +0200

    feat(e30-wu): make it work.

--- src/E-asynchronous-code/30-the-event-loop/e30-1st-the-event-loop-warm-up.js
+++ src/E-asynchronous-code/30-the-event-loop/e30-1st-the-event-loop-warm-up.js
@@ -16,17 +16,19 @@ const collector = [];
 collector.push(1);
 
 
-collector.push(3);
+Promise.resolve().then(() => {
+	collector.push(3);
+})
 
 
 collector.push(2);
 
 queueMicrotask(() => {
-		assertThat(
-			'Collector has elements in proper order',
-			expect => expect(collector).toEqual([1, 2, 3])
-		) //=
-	})
+	assertThat(
+		'Collector has elements in proper order',
+		expect => expect(collector).toEqual([1, 2, 3])
+	) //=
+})
 
 // #2 Task ----------------------------------------------------------
 
@@ -34,15 +36,19 @@ const secondCollector = [];
 
 secondCollector.push(1);
 
-
-secondCollector.push(3);
-
+setTimeout(() => {
+	secondCollector.push(3);
+})
 
 secondCollector.push(2);
 
 queueMicrotask(() => {
-		assertThat(
-			'Second Collector has elements in proper order',
-			expect => expect(secondCollector).toEqual([1, 2])
-		) //=
-	})
+	assertThat(
+		'Second Collector has elements in proper order',
+		expect => expect(secondCollector).toEqual([1, 2])
+	) //=
+})
+
+setTimeout(() => {
+	console.log(secondCollector);
+})
