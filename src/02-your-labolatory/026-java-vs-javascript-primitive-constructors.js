6b94905ad48ea80443e11c17dcf40d71a42f043b
Jabi <michal.jabi@gmail.com>
2025-06-24 09:18:05 +0200

    explain(026): Java and JavaScript provide Constructors for some primitives.

---
+++ src/02-your-labolatory/026-java-vs-javascript-primitive-constructors.js
@@ -0,0 +1,30 @@
+
+const myNumber = 900.128475165;
+
+console.log(Number('678'))
+console.log(Number('suwgy'))
+console.log(typeof Number('suwgy'))
+
+console.log(NaN === NaN)
+console.log(isNaN(NaN))
+console.log(isNaN(undefined))
+
+console.log(Number.isNaN(NaN))
+console.log(Number.isNaN(undefined))
+
+console.log(String(457890))
+console.log(Boolean(0))
+console.log(Boolean(1))
+console.log(Boolean(-1))
+console.log(Boolean(222))
+console.log(Boolean({}))
+console.log(Boolean(NaN))
+
+console.log('?' + '2344');
+console.log(+'2344');
+
+console.log(!!'2344');
+
+console.log(new Number())
+
+console.log(myNumber.toFixed(2))
