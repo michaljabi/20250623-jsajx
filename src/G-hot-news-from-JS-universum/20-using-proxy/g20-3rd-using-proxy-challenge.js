9053ef57503455554fc860348a8b1bfce3009732
Jabi <michal.jabi@gmail.com>
2025-06-25 14:10:50 +0200

    explain(g20-ch): make it more valueable

--- src/G-hot-news-from-JS-universum/20-using-proxy/g20-3rd-using-proxy-challenge.js
+++ src/G-hot-news-from-JS-universum/20-using-proxy/g20-3rd-using-proxy-challenge.js
@@ -1,4 +1,6 @@
 import { assertThat } from '../../j4b1-assert.js'
+// import EventEmitter from 'mitt'
+import { EventEmitter } from 'events';
 /**
  * g20-using-proxy
  * Challenge
@@ -10,11 +12,17 @@ import { assertThat } from '../../j4b1-assert.js'
  * - Kod można zmieniać tylko w środku handlera przekazanego do Proxy
  */
 
+export const emitter = new EventEmitter();
+
 const shouterObject = new Proxy({}, {
 	// #Reguła:
 	// Kod piszemy tylko tutaj:
 	set(target, property, value) {
-		target[property] = value + '!';
+		// target[property] = value + '!';
+		Reflect.set(target, property, value + '!')
+
+		// PROVIDER:
+		emitter.emit('change', { target, property, value })
 	},
 	deleteProperty() { }
 })
@@ -22,12 +30,30 @@ const shouterObject = new Proxy({}, {
 // #Reguła:
 // Nie możesz zmieniać kodu poniżej:
 
+// CONSUMER
+emitter.on('change', (event) => {
+	console.log(event);
+})
+
+
+// CONSUMER 2
+emitter.on('change', (event) => {
+	console.log(event);
+})
+
+// CONSUMER 3
+emitter.on('change', (event) => {
+	console.log(event);
+})
+
 shouterObject.name = 'Michal';
 shouterObject.lastName = 'Kowalsky';
 shouterObject.age = 33;
 shouterObject.nothing = 'will-stay'
 
 delete shouterObject.nothing;
+
+// emitter.emit('change', 'TROLL')
 
 assertThat(
 	'Should have name shouted',
