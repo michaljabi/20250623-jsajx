23543a1f9660cd7081449e29b2d1611d39712b0f
Jabi <michal.jabi@gmail.com>
2025-06-25 09:50:29 +0200

    explain(029): iterable collections + difference for...of / for...in

---
+++ src/02-your-labolatory/029-iterables.js
@@ -0,0 +1,55 @@
+const simplest = 'HELLO WORLD'
+
+console.log(simplest[0])
+console.log(simplest[100])
+console.log(simplest.length)
+console.log(simplest[10])
+
+for (const item of simplest) {
+    console.log(item);
+}
+
+
+const simple = [1, 2, 3, 4, 'A', true];
+
+simple.forEach((e, idx) => {
+    console.log(e);
+    console.log(idx);
+})
+
+for (let idx = 0; idx < simple.length; idx++) {
+    console.log(simple[idx]);
+    console.log(idx);
+}
+
+for (const item of simple) {
+    console.log(item);
+}
+
+// Do czego służy pętla for...in
+for (const item in simple) {
+    console.log(item);
+}
+// odp: DO OBIEKTÓW ! innych niż Array!
+
+const user = { name: "Michał", 1: 'hello' }
+
+for (const myKey in user) {
+    console.log(myKey)
+}
+
+const arrayLike = { 0: 'a', 1: 'b', length: 2 };
+
+for (const values of Array.from(arrayLike)) {
+    console.log(values);
+}
+
+// to nie zadziała bo brakuje [Symbol.iterable]
+// TODO. dostarcz Symbol.iterable + generator - żeby działało!
+for (const values of [...arrayLike]) {
+    console.log(values);
+}
+
+// Obiekty ArrayLike w przeglądarce
+// Array.from(document.querySelectorAll('a')).map(() => 'troll')
+// [...document.querySelectorAll('a')].map(() => 'troll')
